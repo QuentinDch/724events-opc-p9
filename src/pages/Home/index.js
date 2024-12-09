289fb9657271eccdd07f6c001db478b537f41345
Quentin <quentin.caflo@gmail.com>
2024-12-09 14:28:58 +0100

    fix: handle missing or invalid data for EventCard component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -114,13 +114,16 @@ const Page = () => {
       <footer className="row">
         <div className="col presta">
           <h3>Notre derniére prestation</h3>
-          <EventCard
-            imageSrc={last?.cover}
-            title={last?.title}
-            date={new Date(last?.date)}
-            small
-            label={last?.type}
-          />
+          {last && (
+            <EventCard
+              imageSrc={last?.cover}
+              imageAlt={last?.description}
+              title={last?.title}
+              date={new Date(last?.date)}
+              small
+              label={last?.type}
+            />
+          )}
         </div>
         <div className="col contact">
           <h3>Contactez-nous</h3>
