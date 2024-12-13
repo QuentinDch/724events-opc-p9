a1558f9901419849f5b4a6e7e9bb495aefe73f9c
Quentin <quentin.caflo@gmail.com>
2024-12-13 15:43:55 +0100

    test(people-card): add test to check image, name, and position display

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -1,5 +1,6 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import Home from "./index";
+import PeopleCard from "../../components/PeopleCard";
 
 describe("When Form is created", () => {
   it("a list of fields card is displayed", async () => {
@@ -24,21 +25,33 @@ describe("When Form is created", () => {
       await screen.findByText("Message envoyé !");
     });
   });
-
 });
-
 
 describe("When a page is created", () => {
   it("a list of events is displayed", () => {
     // to implement
-  })
-  it("a list a people is displayed", () => {
-    // to implement
-  })
+  });
+  it("a list of people is displayed", () => {
+    render(
+      <PeopleCard
+        imageSrc="http://src-image"
+        imageAlt="image-alt-text"
+        name="test name"
+        position="test position"
+      />
+    );
+    const nameElement = screen.getByText(/test name/);
+    const positionElement = screen.getByText(/test position/);
+    const imageElement = screen.getByTestId("card-image-testid");
+    expect(nameElement).toBeInTheDocument();
+    expect(positionElement).toBeInTheDocument();
+    expect(imageElement).toBeInTheDocument();
+    expect(imageElement.alt).toEqual("image-alt-text");
+  });
   it("a footer is displayed", () => {
     // to implement
-  })
+  });
   it("an event card, with the last event, is displayed", () => {
     // to implement
-  })
+  });
 });
